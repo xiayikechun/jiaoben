8ee33aa6ed70b482d2b8d0bfb537e7bd734ef9bd
xiayikechun <1581487515@qq.com>
2023-03-07 15:49:20 +0800

    Update xintiao.sh

--- xintiao.sh
+++ xintiao.sh
@@ -1,3 +1,8 @@
-* * * * * tag=心跳
-#!/usr/bin/env bash
+/*
+心跳
+项目地址: https://github.com/hex-ci/smzdm_script
+
+cron: * * * * *
+*/
+
 curl https://jk.puguying.cn/api/push/PZlS4ip7c8?status=up&msg=OK&ping=
